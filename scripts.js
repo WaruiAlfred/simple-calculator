aacc6f8546cb2526b55c8a019cd6baeb366487d9
Alfred Warui <alfredwarui17@gmail.com>
2022-07-20 21:51:50 +0300

    create operate function

--- scripts.js
+++ scripts.js
@@ -14,3 +14,18 @@ const multiply = function (num1, num2) {
 const divide = function (num1, num2) {
   return num1 / num2;
 };
+
+const operate = function (operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "Something is missing:Either operator or one of the two required number inputs";
+  }
+};
