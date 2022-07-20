d26038adfa863a440e517b486c8b6f872f4261de
Alfred Warui <alfredwarui17@gmail.com>
2022-07-20 21:28:14 +0300

    create basic calculator functions

---
+++ scripts.js
@@ -0,0 +1,16 @@
+///// Functions
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
